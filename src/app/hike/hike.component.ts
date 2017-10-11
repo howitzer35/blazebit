5a2a47f55b0719891ceff888db6ee2001c167c31
bgriffincoder <bryan.r.griffin@gmail.com>
2017-10-11 08:10:13 -0700

    hiking updates

--- src/app/hike/hike.component.ts
+++ src/app/hike/hike.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HikeComponent implements OnInit {
 
+  private destination: string;
+  private distance: string;
+  private elevation: string;
+  private pass: string;
+  private rating: string;
+  private trailtype: string;
+
   constructor() { }
+
+  stubData = [{
+    destination: "Lingering Trail",
+    distance: "15.45 miles",
+    elevation: "400 ft",
+    pass: "Discover Pass required",
+    rating: 2,
+    trailtype: "Loop"
+  },
+  {
+    destination: "Lundin Peak Trail",
+    distance: "15.45 miles",
+    elevation: "400 ft",
+    pass: "Discover Pass required",
+    rating: 2,
+    trailtype: "Point to Point"
+  },
+  {
+    destination: "Lingering Trail",
+    distance: "15.45 miles",
+    elevation: "400 ft",
+    pass: "Discover Pass required",
+    rating: 2,
+    trailtype: "Up & Down"
+  }
+]
+  
 
   ngOnInit() {
   }
+
+  
 
 }
