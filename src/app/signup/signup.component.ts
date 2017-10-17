7158627f0a62c7e1dfcc0196055b4924200cd7e4
Helena Witzke <helena.witzke@gmail.com>
2017-10-17 13:40:46 -0700

    signup routing updates

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -15,7 +15,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls:  ['./signup.component.css']
 })
 
 export class SignupComponent implements OnInit {
