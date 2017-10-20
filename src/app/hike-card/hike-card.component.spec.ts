60b0ba93673f37a90c0c4f2a6d9863d1f06aab76
Joseph mar <Joseph.mar@libertymutual.com>
2017-10-20 08:24:56 -0700

    cards that we made with curtis

---
+++ src/app/hike-card/hike-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HikeCardComponent } from './trail-card.component';
+
+describe('TrailCardComponent', () => {
+  let component: HikeCardComponent;
+  let fixture: ComponentFixture<HikeCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HikeCardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HikeCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
