06c4a557e026a1efaaa696e3966bea421bc6b165
bgriffincoder <bryan.r.griffin@gmail.com>
2017-10-23 20:43:26 -0700

    add signup form code

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -73,6 +73,18 @@ export class ProfileComponent {
   console.log(e);
 }
 
+//adds 1 hike to list of completed hikes for user
+addHikeToUser(id: number) {
+  this.dataService.manageHikeRecord("users/trails", id)
+    .subscribe(user => this.handleSuccessfulWishlistComplete(user));
+}
+
+private handleSuccessfulWishlistComplete(user: User) {
+  this.successMessage = "Wishlist Hike was successfully added to complete list for user!";
+  this.funFastUserService.refreshUser(user);
+  this.ngOnInit();
+}
+
 deleteWishlistHike(id: number) {
   this.dataService.deleteWishlistRecord("users/trails", id)
     .subscribe(user => this.handleSuccessfulWishDelete(user));
